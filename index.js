0faf30279dab19fea83f6ea5c5d3697341214239
sugiilol <sugii.noob@gmail.com>
2024-04-05 18:00:22 +0200

    nodemailer setuped and functional

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express")
+const nodemailer = require("nodemailer");
+const cors = require("cors")
+
+//express **************************
+
+const app = express()
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Application Node démarrée sur : http://localhost:${port}`)
+})
+
+app.use(express.json()) // for parsing application/json
+app.use(cors())
+
+//nodemailer **************************
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: "sugii.noob@gmail.com",
+      pass: "ypst pcfx mvhi kxhd",
+    },
+  });
+  
+  
+  const mailOptions = {
+      from:{
+          name:'Sebastien Leblanc',
+          adress: "sugii.noob@gmail.com"
+      },
+      to: ["leblanc.sbt@gmail.com"],
+      subject:'test nodemailer - send',
+      text: "hello world",
+      html: "<b>prout</<b>"
+  }
+  
+  const sendMail = async ( transporter, mailOptions) => {
+      try{
+          await transporter.sendMail(mailOptions)
+          console.log("Email envoyé correctement!")
+      }
+      catch (error){
+          console.log(error)
+      }
+  }
+  
+//routes **************************
+
+app.post('/api/postemail/', (req, res, next) => {
+    const emailCreated = req.body  
+    
+    const mailOptions = {
+        from:{
+            name:`${emailCreated.fullName}`,
+            adress: "sugii.noob@gmail.com"
+        },
+        to: ["leblanc.sbt@gmail.com"],
+        subject:`${emailCreated.subject}`,
+        text: `${emailCreated.message}`,
+        html: ` <h1>Email envoyé du portfolio</h1>
+                <h2>${emailCreated.subject}</h2>
+                <h3>${emailCreated.fullName}</h3>
+                <h3>${emailCreated.email}</h3>
+                <h3>${emailCreated.phone}</h3>
+                <p>${emailCreated.message}</p>`
+    }
+
+    sendMail(transporter, mailOptions)
+
+    res.json(emailCreated)
+})
+
